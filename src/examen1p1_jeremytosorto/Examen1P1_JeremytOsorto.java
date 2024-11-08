ceac552104be127473499df5ea847717604f7037
ukken kun <jeremyt_osorto@unitec.edu>
2024-11-08 15:25:47 -0600

    Secon commit

--- src/examen1p1_jeremytosorto/Examen1P1_JeremytOsorto.java
+++ src/examen1p1_jeremytosorto/Examen1P1_JeremytOsorto.java
@@ -31,11 +31,27 @@ public class Examen1P1_JeremytOsorto {
         return desc;
     }
     
+    public static char[] correcto(char[] desc1, char[] cor, char busc){
+        int i,j;
+        
+        for (i=0; i<desc1.length;i++){
+            for(j=0;j<cor.length;j++){
+                if (busc==desc1[j]){
+                    cor[j]=busc;
+                    continue;
+                }
+            }
+        }
+        
+        return cor;
+    }
+    
     public static void descifrarCombinacion(){
         Scanner op=new Scanner(System.in);
+        Scanner bus=new Scanner(System.in);
         int inte, opc, tam, inte1;
         
-        inte1=0;
+        inte1=1;
         inte=0;
         tam=0;
         
@@ -60,13 +76,96 @@ public class Examen1P1_JeremytOsorto {
         char[] cor=new char[tam];
         char[] desc1=crearCombinacion(desc, cor);
         
-        for(int i=0; i<desc1.length;i++){
-            System.out.print(cor[i]+" ");
+        while(inte1<=inte){
+            System.out.println("Intento: "+inte1+"/"+inte+"\nIngrese un carecter en minuscula: ");
+            char busc=bus.next().charAt(0);
+            
+            correcto(desc1,cor, busc);
+            
+            System.out.print("Su progreso: ");
+            
+            for(int i=0; i<desc1.length;i++){
+                System.out.print(cor[i]+" ");
+            }
+            System.out.println("");
+            inte1++;
+            
+            boolean t;
+            t=true;
+            
+            for (int i=0; i<desc1.length;i++){
+                if (desc1[i]==cor[i]){
+                    t=true;
+                }
+                else{
+                    t=false;
+                }
+            }
+            
+            if (t==true){
+                System.out.println("Lo descubriste en "+inte1+" intentos :)");
+                break;
+            }
+        }
+        if (inte1==inte+1){
+            System.out.println("La bomba exploto :(");
+            System.out.println("La cadena era: ");
+            for(int i=0; i<desc1.length;i++){
+                System.out.print(desc1[i]+" ");
+            }
+        }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+    
+    public static int[] crearcombinacion(int[] arr, int[] arr1){
+        Random ran=new Random();
+        int i, rand;
+        
+        for(i=0; i<arr.length;i++){
+            rand=ran.nextInt(1,100);
+            arr[i]=rand;
         }
         
-        while(inte1<=inte){
+        return arr;
+    }
+    
+    public static void rotacionCircular(){
+        Scanner lee= new Scanner(System.in);
+        Scanner le=new Scanner(System.in);
+        int tam;
+        
+        System.out.println("Ingresa el tamaño del arreglo");
+        tam=lee.nextInt();
+        
+        if (tam>5){
+            int[] arr=new int[tam];
+            int[] arr1=new int[tam];
+            arr=crearcombinacion(arr, arr1);
             
+            System.out.print("Arreglo generado: [");
+            for (int i=0; i<arr.length;i++){
+                System.out.print(" "+arr[i]);
+            }
+            System.out.println(" ]");
+            
+            System.out.println("Ingrese la cadena de rotación (formato i|d:número): ");
+            String nu=le.next();
+            
+            String c=nu.substring(0);
+            String d=nu.substring(2);
+            
+            int g=Integer.parseInt(d);
+            char t=c.charAt(0);
+            
+            System.out.println(g+""+t);
         }
+        else{
+            System.out.println("ingrese un valor mayor 5");
+        }
+        
+        
     }
     
     public static void main(String[] args) {
@@ -83,6 +182,7 @@ public class Examen1P1_JeremytOsorto {
                     descifrarCombinacion();
                     break;
                 case 2:
+                    rotacionCircular();
                     break;
                 case 3:
                     break;
